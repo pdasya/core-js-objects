8896dfcf5f1970a95074abd16e7793fff3188b02
Pashkova Daria <dar.pashcovaa@gmail.com>
2024-02-29 16:31:13 +0900

    feat: solve shallowCopy and mergeObjects tasks

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,9 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+function shallowCopy(inputObject) {
+  const copyObject = {};
+  return Object.assign(copyObject, inputObject);
 }
 
 /**
@@ -32,8 +33,14 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objectsArray) {
+  const resultObject = {};
+  objectsArray.forEach((object) => {
+    Object.entries(object).forEach(([key, value]) => {
+      resultObject[key] = (resultObject[key] ? resultObject[key] : 0) + value;
+    });
+  });
+  return resultObject;
 }
 
 /**
