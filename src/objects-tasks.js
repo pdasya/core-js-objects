7756c241049b5c621d2e17b1fa7d45080b39ec1d
Pashkova Daria <dar.pashcovaa@gmail.com>
2024-03-04 15:33:43 +0900

    feat: solve sellTickets task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -162,8 +162,17 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  const ticketPrice = 25;
+  let isSell = true;
+
+  queue.forEach((ticket) => {
+    if (ticket - ticketPrice > 25) {
+      isSell = false;
+    }
+  });
+
+  return isSell;
 }
 
 /**
