8d72551ef4f970cec9aae62cac42d48d1fbf796e
Pashkova Daria <dar.pashcovaa@gmail.com>
2024-03-04 15:13:43 +0900

    feat: solve makeWord task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -123,8 +123,8 @@ function isEmptyObject(obj) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -137,8 +137,15 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const result = Object.entries(lettersObject)
+    .map(([key, value]) => value.map((index) => [key, index]))
+    .flat()
+    .sort((a, b) => a[1] - b[1])
+    .map((item) => item[0])
+    .join('');
+
+  return result;
 }
 
 /**
